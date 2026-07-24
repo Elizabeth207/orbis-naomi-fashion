ad894153197419106ae80fb5f878cff24c24dc5f
Elizabeth207 <naomiorbis@gmail.com>
2026-07-24 17:44:00 -0500

    Actualizar horario de tiendas en stores.ts

--- src/data/stores.ts
+++ src/data/stores.ts
@@ -18,12 +18,12 @@ export const STORES: Store[] = [
     name: "Orbis II",
     slogan: "Moda casual para damas y caballeros.",
     address: "671 Av Lima, Pichanaki, Junín, Perú",
-    hours: "Lun-Vie 9am-10pm, Sáb-Dom 9:30am-6pm",
+    hours: "Lun-Vie 8:00 am - 10:00 pm, Sáb-Dom 9:00 am - 10:00 pm",
     whatsapp: "51916621772",
     hoursDetail: [
       { day: "Lunes a Viernes", time: "8:00 am - 10:00 pm" },
-      { day: "Sábados", time: "9:30 am - 6:00 pm" },
-      { day: "Domingos", time: "9:30 am - 6:00 pm" }
+      { day: "Sábados", time: "9:00 am - 10:00 pm" },
+      { day: "Domingos", time: "9:00 am - 10:00 pm" }
     ],
     mapsQuery: "Orbis II, 671 Av Lima, Pichanaki, Junín, Perú",
     coordinates: { lat: -10.9255969, lng: -74.875049 }
@@ -34,12 +34,12 @@ export const STORES: Store[] = [
     name: "Novias Naomi",
     slogan: "Vestidos y prendas de gala para cada ocasión especial.",
     address: "Bajo Pichanaqui, Chanchamayo, Junín, Perú (coordenadas exactas confirmadas, dirección en texto pendiente)",
-    hours: "Lun-Vie 9am-10pm, Sáb-Dom 9:30am-6pm",
+    hours: "Lun-Dom 9:00 am - 10:00 pm",
     whatsapp: "51916621772",
     hoursDetail: [
       { day: "Lunes a Viernes", time: "9:00 am - 10:00 pm" },
-      { day: "Sábados", time: "9:30 am - 6:00 pm" },
-      { day: "Domingos", time: "9:30 am - 6:00 pm" }
+      { day: "Sábados", time: "9:00 am - 10:00 pm" },
+      { day: "Domingos", time: "9:00 am - 10:00 pm" }
     ],
     mapsQuery: "-10.9257816,-74.8742806",
     coordinates: { lat: -10.9257816, lng: -74.8742806 }
